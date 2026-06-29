c9527677284534495dbcd1ef1b81383b36ca5015
Ciastelix <tenlegitmati@gmail.com>
2026-06-29 18:25:19 +0200

    added path search and counting duplicates

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from collections import Counter
+from pathlib import Path
+
 import xxhash
+
+files = Counter()
 
 
 def hash_file_xxhash(file_path):
@@ -6,11 +11,27 @@ def hash_file_xxhash(file_path):
     with open(file_path, "rb") as f:
         for chunk in iter(lambda: f.read(8192), b""):
             h.update(chunk)
-    return h.hexdigest()
+    files[str(h.hexdigest())] += 1
+
+
+def scan_for_files():
+    pathlist = Path("/")
+    for p in pathlist.glob("*"):
+        for i in p.rglob("*"):
+            try:
+                if i.is_file():
+                    hash_file_xxhash(i)
+            except PermissionError:
+                print("perm den")
+                continue
+            except OSError:
+                print("sys error")
+                continue
 
 
 def main():
-    print(hash_file_xxhash("./uv.lock"))
+    scan_for_files()
+    print(files.most_common(5))
 
 
 if __name__ == "__main__":
